cc2d557efa0d790d60a696566c2ac83103e8a3ac
skitou <chiantigally@gmail.com>
2023-06-11 18:14:09 +0200

    Fixed About command

--- src/main/java/fr/skitou/kanei/slashCommands/About.java
+++ src/main/java/fr/skitou/kanei/slashCommands/About.java
@@ -31,13 +31,13 @@ public class About implements ISlashCommand {
         String timeInHHMMSS = String.format("%02d:%02d:%02d", HH, MM, SS);
 
         EmbedBuilder builder = new EmbedBuilder();
-        builder.setTitle(BotInstance.getJda().getSelfUser().getName()+"Music Bot Infos")
+        builder.setTitle(BotInstance.getJda().getSelfUser().getName() + "Music Bot Infos")
                 .setDescription("This is a music bot made for me and my friends using JDA and lavaplayer.\n Type /help to see all commands available.")
-                .addField("Versions:", "JDA5\nCore:"+BotInstance.getCoreVersion()+"\n"+BotInstance.getJda().getSelfUser().getName()+":"+ KaneiMain.getVersion(), true)
+                .addField("Versions:", "JDA5\nCore:" + BotInstance.getCoreVersion() + "\n" + BotInstance.getJda().getSelfUser().getName() + ":" + KaneiMain.getVersion(), true)
                 .addField("Total servers:", String.valueOf(BotInstance.getJda().getGuilds().size()), true)
                 .addField("Total stream:", "", true)
                 .setColor(QuickColors.LIGHT_BLUE)
-                .setFooter("Running on Java "+ Runtime.version()+" | Uptime:"+ timeInHHMMSS);
+                .setFooter("Running on Java " + Runtime.version() + " | Uptime:" + timeInHHMMSS);
         event.replyEmbeds(builder.build()).queue();
     }
 }
