a3b79b7f3e10933c7e220e0ac247c1000b4dbe46
skitou <chiantigally@gmail.com>
2023-06-11 18:14:26 +0200

    Added GuildMusicSettings

---
+++ src/main/java/fr/skitou/kanei/databaseentities/GuildMusicSettings.java
@@ -0,0 +1,34 @@
+package fr.skitou.kanei.databaseentities;
+
+import fr.skitou.botcore.hibernate.Database;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Entity
+@Table(name = "guildMusicSettings")
+public class GuildMusicSettings {
+    @Id
+    @Getter
+    @Setter(AccessLevel.PRIVATE)
+    private long guild;
+
+    @Getter
+    @Setter(AccessLevel.PRIVATE)
+    private int volume;
+
+    public GuildMusicSettings(long guild, int volume) {
+        this.guild = guild;
+        this.volume = volume;
+
+        Database.saveOrUpdate(this);
+    }
+
+    protected GuildMusicSettings() {
+    }
+}
