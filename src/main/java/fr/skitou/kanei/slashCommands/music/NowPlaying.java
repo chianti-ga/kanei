7d35ea8ea64e8338889620d8c937fd1ada0a4e54
skitou <chiantigally@gmail.com>
2023-06-11 23:48:26 +0200

    Added NowPlaying slash command

---
+++ src/main/java/fr/skitou/kanei/slashCommands/music/NowPlaying.java
@@ -0,0 +1,28 @@
+package fr.skitou.kanei.slashCommands.music;
+
+import fr.skitou.botcore.slashcommand.ISlashCommand;
+import fr.skitou.kanei.KaneiMain;
+import fr.skitou.kanei.lavautils.MusicManager;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class NowPlaying implements ISlashCommand {
+    @Override
+    public @NotNull String getName() {
+        return "nowplaying";
+    }
+
+    @Override
+    public @NotNull String getHelp() {
+        return "Show current track";
+    }
+
+    @Override
+    public void onCommandReceived(SlashCommandInteractionEvent event) {
+        if (MusicManager.guildMusics.containsKey(event.getGuild().getIdLong())) {
+            if (MusicManager.guildMusics.get(event.getGuild().getIdLong()).player.getPlayingTrack() != null) {
+                event.replyEmbeds(MusicManager.guildMusics.get(event.getGuild().getIdLong()).scheduler.nowPlaying()).queue();
+            } else event.reply(KaneiMain.getLangBundle().getString("music.nothingplaying")).queue();
+        } else event.reply(KaneiMain.getLangBundle().getString("music.nothingplaying")).queue();
+    }
+}
