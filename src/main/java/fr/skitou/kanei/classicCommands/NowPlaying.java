ccb994ab83753a2bf0fdda26482c6d2e6a6d19ba
skitou <chiantigally@gmail.com>
2023-06-11 18:14:39 +0200

    Added NowPlaying classic command

---
+++ src/main/java/fr/skitou/kanei/classicCommands/NowPlaying.java
@@ -0,0 +1,23 @@
+package fr.skitou.kanei.classicCommands;
+
+import fr.skitou.botcore.commands.AbstractCommand;
+import fr.skitou.botcore.commands.CommandReceivedEvent;
+import fr.skitou.kanei.music.MusicManager;
+import org.jetbrains.annotations.NotNull;
+
+public class NowPlaying extends AbstractCommand {
+    @Override
+    public @NotNull String getCommand() {
+        return "nowplaying";
+    }
+
+    @Override
+    public @NotNull String getName() {
+        return "nowplaying";
+    }
+
+    @Override
+    public void onCommandReceived(CommandReceivedEvent event) {
+        event.getChannel().sendMessageEmbeds(MusicManager.guildMusics.get(event.getGuild().getIdLong()).scheduler.nowPlaying()).queue();
+    }
+}
