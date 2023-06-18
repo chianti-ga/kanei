5cf372313ff3e397dfeaa8e73bc0e0f019f4fdf7
skitou <chiantigally@gmail.com>
2023-06-18 18:12:59 +0200

    Added Clear command

---
+++ src/main/java/fr/skitou/kanei/slashCommands/music/Clear.java
@@ -0,0 +1,44 @@
+package fr.skitou.kanei.slashCommands.music;
+
+import fr.skitou.botcore.slashcommand.ISlashCommand;
+import fr.skitou.kanei.KaneiMain;
+import fr.skitou.kanei.lavautils.GuildMusic;
+import fr.skitou.kanei.lavautils.MusicManager;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class Clear implements ISlashCommand {
+    @Override
+    public @NotNull String getName() {
+        return "clear";
+    }
+
+    @Override
+    public @NotNull String getHelp() {
+        return "Clear the current queue.";
+    }
+
+    @Override
+    public void onCommandReceived(SlashCommandInteractionEvent event) {
+        event.deferReply(false).queue();
+
+        if (!event.getMember().getVoiceState().inAudioChannel()) {
+            event.getHook().sendMessage(KaneiMain.getLangBundle().getString("music.notinchanel")).queue();
+            return;
+        }
+
+        GuildMusic guildMusic;
+
+        if (MusicManager.guildMusics.containsKey(event.getGuild().getIdLong())) {
+            guildMusic = MusicManager.guildMusics.get(event.getGuild().getIdLong());
+        } else {
+            event.getHook().sendMessage(KaneiMain.getLangBundle().getString("music.nothingplaying")).queue();
+            return;
+        }
+
+        if (guildMusic.scheduler.getQueue().isEmpty()) {
+            guildMusic.scheduler.clearQueue();
+            event.getHook().sendMessage(KaneiMain.getLangBundle().getString("music.clearedqueue")).queue();
+        } else event.getHook().sendMessage(KaneiMain.getLangBundle().getString("music.emptyqueue")).queue();
+    }
+}
