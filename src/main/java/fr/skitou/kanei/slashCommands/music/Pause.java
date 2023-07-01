618582afa3412c992e3d65c240f53caab2816ef1
skitou <chiantigally@gmail.com>
2023-07-01 03:30:09 +0200

    Added pause command

---
+++ src/main/java/fr/skitou/kanei/slashCommands/music/Pause.java
@@ -0,0 +1,51 @@
+package fr.skitou.kanei.slashCommands.music;
+
+import fr.skitou.botcore.slashcommand.ISlashCommand;
+import fr.skitou.kanei.KaneiMain;
+import fr.skitou.kanei.lavautils.GuildMusic;
+import fr.skitou.kanei.lavautils.MusicManager;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class Pause implements ISlashCommand {
+    @Override
+    public @NotNull String getName() {
+        return null;
+    }
+
+    @Override
+    public @NotNull String getHelp() {
+        return null;
+    }
+
+    @Override
+    public void onCommandReceived(SlashCommandInteractionEvent event) {
+        event.deferReply(false).queue();
+
+        if (!event.getMember().getVoiceState().inAudioChannel()) {
+            event.getHook().sendMessage(KaneiMain.getLangBundle().getString("music.notinchanel")).queue();
+            return;
+        }
+
+        GuildMusic guildMusic;
+
+        if (MusicManager.guildMusics.containsKey(event.getGuild().getIdLong())) {
+            guildMusic = MusicManager.guildMusics.get(event.getGuild().getIdLong());
+        } else {
+            event.getHook().sendMessage(KaneiMain.getLangBundle().getString("music.nothingplaying")).queue();
+            return;
+        }
+
+        if (guildMusic.player.getPlayingTrack() == null) {
+            event.getHook().sendMessage(KaneiMain.getLangBundle().getString("music.nothingplaying")).queue();
+            return;
+        }
+        if (guildMusic.player.isPaused()) {
+            event.getHook().sendMessage(KaneiMain.getLangBundle().getString("music.resumed")).queue();
+            guildMusic.player.setPaused(false);
+        } else {
+            event.getHook().sendMessage(KaneiMain.getLangBundle().getString("music.paused")).queue();
+            guildMusic.player.setPaused(true);
+        }
+    }
+}
