8305831c9cbaf34a6f3c1b9a72d340fc531f66ec
skitou <chiantigally@gmail.com>
2023-07-01 03:33:30 +0200

    Added Leave command

---
+++ src/main/java/fr/skitou/kanei/slashCommands/music/Leave.java
@@ -0,0 +1,36 @@
+package fr.skitou.kanei.slashCommands.music;
+
+import fr.skitou.botcore.slashcommand.ISlashCommand;
+import fr.skitou.kanei.KaneiMain;
+import fr.skitou.kanei.lavautils.MusicManager;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class Leave implements ISlashCommand {
+
+    @Override
+    public @NotNull String getName() {
+        return null;
+    }
+
+    @Override
+    public @NotNull String getHelp() {
+        return null;
+    }
+
+    @Override
+    public void onCommandReceived(SlashCommandInteractionEvent event) {
+        event.deferReply(false).queue();
+
+        if (!event.getMember().getVoiceState().inAudioChannel()) {
+            event.getHook().sendMessage(KaneiMain.getLangBundle().getString("music.notinchanel")).queue();
+            return;
+        }
+
+        if (MusicManager.guildMusics.containsKey(event.getGuild().getIdLong())) {
+            MusicManager.guildMusics.get(event.getGuild().getIdLong()).destroy();
+        } else {
+            event.getHook().sendMessage(KaneiMain.getLangBundle().getString("music.nothingplaying")).queue();
+        }
+    }
+}
