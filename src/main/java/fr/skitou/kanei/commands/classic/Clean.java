f4a78035d98b9dbea02318c216526c29b1a36690
skitou <chiantigally@gmail.com>
2023-11-13 22:52:40 +0100

    Added Clean command

---
+++ src/main/java/fr/skitou/kanei/commands/classic/Clean.java
@@ -0,0 +1,27 @@
+package fr.skitou.kanei.commands.classic;
+
+import fr.skitou.botcore.commands.classic.AbstractCommand;
+import fr.skitou.botcore.commands.classic.CommandReceivedEvent;
+import net.dv8tion.jda.api.entities.Message;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class Clean extends AbstractCommand {
+    @Override
+    public @NotNull String getCommand() {
+        return "clean";
+    }
+
+    @Override
+    public @NotNull String getName() {
+        return "clean";
+    }
+
+    @Override
+    public void onCommandReceived(CommandReceivedEvent event) {
+        List<Message> msgs = event.getChannel().getHistory().retrievePast(Integer.parseInt(event.getArgs().getFirst())).complete();
+        event.getChannel().purgeMessages(msgs);
+
+    }
+}
