9c001faf2091f3fe8dd78be6143032ad4cda3c63
skitou <chiantigally@gmail.com>
2023-11-13 22:16:01 +0100

    Funny command

---
+++ src/main/java/fr/skitou/kanei/commands/classic/Deaf.java
@@ -0,0 +1,33 @@
+package fr.skitou.kanei.commands.classic;
+
+import fr.skitou.botcore.commands.classic.AbstractCommand;
+import fr.skitou.botcore.commands.classic.CommandReceivedEvent;
+import fr.skitou.botcore.utils.IsSenderAllowed;
+import net.dv8tion.jda.api.entities.Member;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Predicate;
+
+public class Deaf extends AbstractCommand {
+    @Override
+    public @NotNull String getCommand() {
+        return "deaf";
+    }
+
+    @Override
+    public @NotNull String getName() {
+        return "deaf";
+    }
+
+    @Override
+    public void onCommandReceived(CommandReceivedEvent event) {
+        if (event.getArgs().getFirst().equals("un")){
+            event.getArgs().stream().skip(1).forEach(s -> event.getGuild().getMemberById(s).mute(false).queue());
+        }else event.getArgs().forEach(s -> event.getGuild().getMemberById(s).mute(false).queue());
+    }
+
+    @Override
+    public Predicate<Member> isSenderAllowed() {
+        return IsSenderAllowed.BotAdmin;
+    }
+}
