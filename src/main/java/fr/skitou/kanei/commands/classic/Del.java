0bd3ca3bac9790653372fc1ee0311ab868eb8866
skitou <chiantigally@gmail.com>
2023-11-13 22:52:54 +0100

    Added Del command

---
+++ src/main/java/fr/skitou/kanei/commands/classic/Del.java
@@ -0,0 +1,22 @@
+package fr.skitou.kanei.commands.classic;
+
+import fr.skitou.botcore.commands.classic.AbstractCommand;
+import fr.skitou.botcore.commands.classic.CommandReceivedEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class Del extends AbstractCommand {
+    @Override
+    public @NotNull String getCommand() {
+        return "del";
+    }
+
+    @Override
+    public @NotNull String getName() {
+        return "del";
+    }
+
+    @Override
+    public void onCommandReceived(CommandReceivedEvent event) {
+        event.getArgs().forEach(s -> event.getChannel().deleteMessageById(s).queue());
+    }
+}
