a7c253bb5206395b2ffa12fcd99c3b5940b99535
chianti-ga <chiantigally@gmail.com>
2026-03-19 05:27:31 +0100

    fix

--- src/main/java/fr/skitou/kanei/lava/GuildMusic.java
+++ src/main/java/fr/skitou/kanei/lava/GuildMusic.java
@@ -13,9 +13,9 @@ import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
 import dev.lavalink.youtube.YoutubeAudioSourceManager;
 import dev.lavalink.youtube.YoutubeSourceOptions;
-import dev.lavalink.youtube.clients.AndroidVrWithThumbnail;
 import dev.lavalink.youtube.clients.IosWithThumbnail;
 import dev.lavalink.youtube.clients.MusicWithThumbnail;
+import dev.lavalink.youtube.clients.Tv;
 import dev.lavalink.youtube.clients.WebWithThumbnail;
 import fr.skitou.kanei.core.BotInstance;
 import fr.skitou.kanei.core.Config;
@@ -122,7 +122,7 @@ public class GuildMusic {
                 .setAllowDirectVideoIds(true)
                 .setAllowDirectPlaylistIds(true);
 
-        YoutubeAudioSourceManager sourceManager = new YoutubeAudioSourceManager(options, new WebWithThumbnail(), new MusicWithThumbnail(), new IosWithThumbnail(), new AndroidVrWithThumbnail());
+        YoutubeAudioSourceManager sourceManager = new YoutubeAudioSourceManager(options, new Tv(), new WebWithThumbnail(), new MusicWithThumbnail(), new IosWithThumbnail());
         String refreshToken = Config.CONFIG.getPropertyOrDefault("oauth.refresh");
         if (refreshToken.equals("null") || refreshToken.isBlank()) {
             sourceManager.useOauth2(null, false);
